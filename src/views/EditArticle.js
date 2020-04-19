a8d1f8dff94a06d753b3db58b62cee5ac3a84b45
poposhca <granpoposhca@gmail.com>
2020-04-19 12:36:35 -0500

    Add and test QuillJS.

--- src/views/EditArticle.js
+++ src/views/EditArticle.js
@@ -1,16 +1,36 @@
 import React from 'react'
+import Quill from 'quill'
 
-const EditArticle = () => (
-    <section className="section is-medium">
-        <h1 className="title">Articulo</h1>
-        <div className="field">
-            <input type="text" className="input is-medium" placeholder="Tutulo" />
-        </div>
-        <div className="field">
-            <label className="subtitle">Contenido</label>
-            <input type="text" className="input" />
-        </div>
-    </section>
-)
+const EditArticle = () => {
+    const [quillEditor, setQuillEditor] = React.useState(null)
+    React.useEffect(() => {
+        const newQuillEditor = new Quill('#editor', {
+            modules: {
+                toolbar: [
+                    ['image', 'code-block']
+                ]
+            },
+            theme: 'snow',
+        })
+        setQuillEditor(newQuillEditor);
+    }, [])
+    const getDelta = () => {
+        const delta = quillEditor.getContents()
+        console.log(delta)
+    }
+    return (
+        <section className="section is-medium">
+            <h1 className="title">Articulo</h1>
+            <div className="field">
+                <input type="text" className="input is-medium" placeholder="Tutulo"/>
+            </div>
+            <div className="field">
+                <label className="subtitle">Contenido</label>
+                <div id="editor" />
+                <input type="button" onClick={() => getDelta()}>Test</input>
+            </div>
+        </section>
+    )
+}
 
 export default EditArticle
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
     mode: "production",
@@ -36,8 +36,8 @@ module.exports = {
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
+    /*externals: {
         "react": "React",
         "react-dom": "ReactDOM"
-    }
-};
+    }*/
+}
